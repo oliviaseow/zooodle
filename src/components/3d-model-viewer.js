b8851147d6655c60f9fb7a7a79a2dd1c011ad3e0
Olivia <oliviaseow@gmail.com>
2018-12-06 02:03:36 -0800

    changes to 3D rendering

--- src/components/3d-model-viewer.js
+++ src/components/3d-model-viewer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import * as THREE from 'three'
 
+THREE.ImageUtils.crossOrigin = 'use-credentials';
+
 class ModelViewer extends Component {
   componentDidMount() {
     const width = this.mount.clientWidth
@@ -18,15 +20,66 @@ class ModelViewer extends Component {
     //ADD CUBE
     const geometry = new THREE.BoxGeometry(1, 1, 1)
     const material = new THREE.MeshBasicMaterial({ color: '#433F81' })
-    this.cube = new THREE.Mesh(geometry, material)
-    this.scene.add(this.cube)
+    //this.cube = new THREE.Mesh(geometry, material)
+    //this.scene.add(this.cube)
 
     //load url
-    THREE.Cache.enabled = true
-    this.load()
+
+    var loader = new THREE.ObjectLoader();
+
+    loader.crossOrigin = 'use-credentials';
+
+    loader.load(
+      // resource URL
+      "http://thingiverse-production.s3.amazonaws.com/threejs_json/70/42/c4/f6/d6/c96461c9bearing_20130404-23308-1fcl0el-0.js",
+
+      // onLoad callback
+      // Here the loaded data is assumed to be an object
+      function ( obj ) {
+        this.scene.add( obj );
+      },
+      /*let mesh = 0;
+
+      object.traverse((node) => {
+        if (node.isMesh) mesh = node;
+      });
+
+      mesh.material.morphTargets = true;
+
+      this.scene.add( mesh );
+      },*/
+
+      // onProgress callback
+      function ( xhr ) {
+        console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+      },
+
+      // onError callback
+      function ( err ) {
+        console.error( 'An error happened' );
+      }
+    );
+
+/*   let loader = new THREE.JSONLoader();
+    let loadCallback = function ( geometry, materials ) {
+          var obj, i;
+          for ( i = this.scene.children.length - 1; i >= 0 ; i -- ) {
+              obj = this.scene.children[ i ];
+              if ( obj !== this.camera) {
+                  this.scene.remove(obj);
+              }
+          }
+          var mesh = new THREE.Mesh( geometry, material );
+          this.scene.add( mesh );
+        };
+
+    var url = "http://thingiverse-production.s3.amazonaws.com/threejs_json/70/42/c4/f6/d6/c96461c9bearing_20130404-23308-1fcl0el-0.js";
+    
+    loader.load(url, loadCallback);*/
 
     this.start()
   }
+
   componentWillUnmount() {
     this.stop()
     this.mount.removeChild(this.renderer.domElement)
@@ -40,13 +93,13 @@ class ModelViewer extends Component {
     cancelAnimationFrame(this.frameId)
   }
   animate = () => {
-    // this.model.rotation.x += 0.01
-    // this.model.rotation.y += 0.01
+    //this.cube.rotation.x += 0.01
+    //this.cube.rotation.y += 0.01
     this.renderScene()
     this.frameId = window.requestAnimationFrame(this.animate)
   }
 
-  load = () => {
+/*  load = () => {
     const renderToScene = this.addToRenderer
     const loader = new THREE.ObjectLoader()
     // loader.setResponseType('json')
@@ -73,7 +126,7 @@ class ModelViewer extends Component {
 
   addToRenderer = geom => {
     this.scene.add(geom)
-  }
+  }*/
 
   renderScene = () => {
     this.renderer.render(this.scene, this.camera)
